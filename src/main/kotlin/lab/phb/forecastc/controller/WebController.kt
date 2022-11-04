48c6422001e68130406f028335a3662c6ee55be7
tamami <tamami.oka@gmail.com>
2022-11-04 14:47:37 +0700

    update controller

--- src/main/kotlin/lab/phb/forecastc/controller/WebController.kt
+++ src/main/kotlin/lab/phb/forecastc/controller/WebController.kt
@@ -1,12 +1,16 @@
 package lab.phb.forecastc.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class WebController {
 
     @RequestMapping("/")
-    fun getRoot() = "index"
+    fun getRoot(model: Model): String {
+        model.addAttribute("param", String())
+      return "index"
+    }
 
 }
